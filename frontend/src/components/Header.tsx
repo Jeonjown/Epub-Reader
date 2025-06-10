5a982f24dcc52394b8479c8b03d6dbab0c1f84a5
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2025-06-10 10:33:07 +0800

    linked github account

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,20 @@
+import { FaGithub } from "react-icons/fa6";
+
 const Header = () => {
   return (
     <>
       <header className="border-b-2 p-5">
         <div className="flex justify-between">
           <h2 className="text-text">BATS</h2>
-          <h2 className="text-text">github</h2>
+          <div className="flex space-x-3">
+            <a
+              href="https://github.com/Jeonjown"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub size={30} />
+            </a>
+          </div>
         </div>
       </header>
     </>
