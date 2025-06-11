401a8e4437e3317c0d56fde3370320da246ab154
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2025-06-11 22:04:04 +0800

    add basic book marking functionality and changed the header

--- frontend/src/pages/EpubReader.tsx
+++ frontend/src/pages/EpubReader.tsx
@@ -1,6 +1,6 @@
 import type { Rendition } from "epubjs";
 import { useEffect, useRef, useState } from "react";
-import { FaBook } from "react-icons/fa6";
+import { FaBook, FaBookBookmark, FaBookmark } from "react-icons/fa6";
 import { ReactReader } from "react-reader";
 import Dictionary from "../components/Dictionary";
 
@@ -11,6 +11,8 @@ interface EpubReaderProps {
 const EpubReader = ({ file }: EpubReaderProps) => {
   const [location, setLocation] = useState<string | number>(0);
   const [buffer, setBuffer] = useState<ArrayBuffer | null>(null);
+
+  const [bookmarkMode, setBookmarkMode] = useState(false);
   const [dictionaryMode, setDictionaryMode] = useState(false);
   const [selectedText, setSelectedText] = useState("");
   const renditionRef = useRef<Rendition | null>(null);
@@ -50,6 +52,17 @@ const EpubReader = ({ file }: EpubReaderProps) => {
 
   return (
     <div className="relative h-[80vh] rounded-lg">
+      <div className="flex items-center space-x-1">
+        <FaBookBookmark
+          className={`opacity-15 transition-transform hover:scale-110 hover:cursor-pointer hover:opacity-100`}
+          onClick={() => {
+            const bookmark = localStorage.getItem("bookmark-location");
+            setLocation(bookmark);
+          }}
+        />
+        <p>go to last bookmark </p>
+      </div>
+
       {/* Dictionary mode toggle */}
       <Dictionary word={selectedText} />
       <div className="group absolute top-5 right-5 z-10 flex items-center space-x-2">
@@ -58,7 +71,7 @@ const EpubReader = ({ file }: EpubReaderProps) => {
         >
           Highlight a word
         </p>
-        <div className="relative">
+        <div className="relative flex">
           <FaBook
             size={22}
             className={`transition-transform ${
@@ -78,6 +91,14 @@ const EpubReader = ({ file }: EpubReaderProps) => {
             Dictionary Mode
           </span>
         </div>
+        <FaBookmark
+          size={22}
+          className={`transition-transform hover:scale-110 hover:cursor-pointer ${localStorage.getItem("bookmark-location") === location ? "opacity-100" : "opacity-15"}`}
+          onClick={() => {
+            setBookmarkMode((prev) => !prev);
+            localStorage.setItem("bookmark-location", location);
+          }}
+        />
       </div>
 
       {/* EPUB Reader */}
